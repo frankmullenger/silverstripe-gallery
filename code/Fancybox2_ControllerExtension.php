9aaaadd6d1809b054d0dc047117eb789fe82b05e
Sander van Dragt <sander.vandragt@gmail.com>
2014-02-06 14:42:54 +0000

    Combine requirements for improved performance.

--- code/Fancybox2_ControllerExtension.php
+++ code/Fancybox2_ControllerExtension.php
@@ -3,11 +3,15 @@
 class Fancybox2_ControllerExtension extends DataExtension {
 
 	public function onAfterInit() {
-		Requirements::javascript('gallery/javascript/fancybox2/jquery.fancybox.js');
-		Requirements::javascript('gallery/javascript/fancybox2/GalleryPage.js');
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js'); 
 
-		Requirements::css('gallery/css/fancybox2/jquery.fancybox.css');
+		Requirements::combine_files('fancebox2.js', array(
+			'gallery/javascript/fancybox2/jquery.fancybox.js',
+			'gallery/javascript/fancybox2/GalleryPage.js',
+		));
+		Requirements::combine_files('fancybox2.css', array(
+			'gallery/css/fancybox2/jquery.fancybox.css',
+		));
 
 	}
 	
