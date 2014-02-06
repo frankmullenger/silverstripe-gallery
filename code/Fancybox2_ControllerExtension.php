a7b105200026e7b9fa71c5ad0dee2b498cb60165
Sander van Dragt <sander.vandragt@gmail.com>
2014-02-06 14:41:55 +0000

    Avoid double jQuery includes by using the included framework library.

--- code/Fancybox2_ControllerExtension.php
+++ code/Fancybox2_ControllerExtension.php
@@ -3,9 +3,9 @@
 class Fancybox2_ControllerExtension extends DataExtension {
 
 	public function onAfterInit() {
-		Requirements::javascript('gallery/javascript/fancybox2/jquery-1.7.1.min.js');
 		Requirements::javascript('gallery/javascript/fancybox2/jquery.fancybox.js');
 		Requirements::javascript('gallery/javascript/fancybox2/GalleryPage.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js'); 
 
 		Requirements::css('gallery/css/fancybox2/jquery.fancybox.css');
 
