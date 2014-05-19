c47fe758ba10b62eeeefbc9799406f8e7afb02c3
Morven Lewis-Everley <morven@i-lateral.com>
2014-05-19 16:50:03 +0100

    Update GalleryConfig.php
    Update so that we can access Silverstripe config properly

--- code/config/GalleryConfig.php
+++ code/config/GalleryConfig.php
@@ -7,7 +7,7 @@
  * The static variables are called by Gallery Page Controller on initilisation
  *
  */
-class GalleryConfig {
+class GalleryConfig extends Object {
     /**
      * Max width of the gallery image
      *
